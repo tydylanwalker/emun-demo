8099bab9d854966afa962862ec5c8e504c28fd3a
Ty Walker <tydylanwalker@gmail.com>
2024-10-23 00:05:32 -0500

    view orders table with formatted data

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,15 +8,17 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { orders } from "@/data/orders";
-import { text } from "stream/consumers";
+import { Typography } from "@mui/material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
+    whiteSpace: "nowrap", // Prevent text wrapping in header cells
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
+    whiteSpace: "nowrap", // Prevent text wrapping in body cells
   },
 }));
 
@@ -24,114 +26,172 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
-  // hide last border
   "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
-const tableHeaders = [
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getValueByPath = (obj: any, path: string) => {
+  return path
+    .replace(/\[(\d+)\]/g, '.$1')  
+    .split('.')                   
+    .reduce((acc, key) => acc && acc[key], obj);  
+};
+
+interface ITableHeaders {
+  text: string;
+  fieldName: string; // could do keyof Order but if nested values we can't
+  align?: 'left' | 'right' | 'center';
+  type?: 'currency' | 'date' | 'status'
+}
+
+const tableHeaders: ITableHeaders[] = [
   {
-    text: "id",
-    align: "right",
+    text: "Order #",
+    align: "left",
     fieldName: "id",
   },
   {
     text: "Customer",
-    align: "right",
-    fieldName: "id",
+    align: "left",
+    fieldName: "companyName",
   },
   {
     text: "PO #",
     align: "right",
-    fieldName: "id",
+    fieldName: "purchaseOrder",
   },
   {
     text: "Source",
-    align: "right",
-    fieldName: "id",
+    align: "left",
+    fieldName: "orderSourceLabel",
   },
   {
     text: "Vendor",
-    align: "right",
-    fieldName: "id",
+    align: "left",
+    fieldName: "vendorLabel",
   },
   {
     text: "Amount",
     align: "right",
-    fieldName: "id",
+    fieldName: "grandTotal",
+    type: 'currency',
   },
   {
     text: "Balance",
     align: "right",
-    fieldName: "id",
+    fieldName: "balance",
+    type: 'currency',
   },
   {
     text: "Order Date",
     align: "right",
-    fieldName: "id",
+    fieldName: "orderedOn",
+    type: 'date'
   },
   {
     text: "Ship Date",
     align: "right",
-    fieldName: "id",
+    fieldName: "shipOn",
+    type: 'date'
   },
   {
     text: "Ship City",
-    align: "right",
-    fieldName: "id",
+    align: "left",
+    fieldName: "shipCity",
   },
   {
     text: "Ship State",
-    align: "right",
-    fieldName: "id",
+    align: "left",
+    fieldName: "shipState",
   },
   {
     text: "Rep",
-    align: "right",
-    fieldName: "id",
+    align: "left",
+    fieldName: "currentRepName",
   },
   {
     text: "Writing Rep",
-    align: "right",
-    fieldName: "id",
+    align: "left",
+    fieldName: "writingRepName",
   },
   {
     text: "Generated From",
-    align: "right",
-    fieldName: "id",
+    align: "left",
+    fieldName: "generatedFrom",
   },
   {
     text: "Status",
-    align: "right",
-    fieldName: "id",
+    align: "center",
+    fieldName: "status",
+    type: 'status',
   },
 ];
 
 export function CustomizedTables() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+    <TableContainer component={Paper} style={{ overflowX: "auto" }}>
+      <Table>
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            {tableHeaders.map((header, index) => (
+              <StyledTableCell key={index} align={header.align || "left"}>
+                {header.text}
+              </StyledTableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {orders.results.map((order, index) => (
-            <StyledTableRow key={index}>
-              <StyledTableCell component="th" scope="row">
-                {order.companyName}
-              </StyledTableCell>
-              {/* <StyledTableCell align="right">{order.}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell> */}
+          {orders.results.map((order, rowIndex) => (
+            <StyledTableRow key={rowIndex}>
+              {tableHeaders.map((header, index) => {
+                const { fieldName, align, type } = header;
+                const value = getValueByPath(order, fieldName);
+                let content = value;
+                let typographyStyles = {};
+
+                switch (type) {
+                  case "date":
+                    const date = new Date(value);
+                    content = isNaN(date.getTime())
+                      ? "-"
+                      : `${(date.getMonth() + 1).toString().padStart(2, "0")}/${date
+                          .getDate()
+                          .toString()
+                          .padStart(2, "0")}/${date.getFullYear()}`;
+                    break;
+                  case "currency":
+                    content = "$" + Number(value).toFixed(2);
+                    break;
+                  case "status":
+                    // TODO can def clean this up but works for time being
+                    typographyStyles = {
+                      bgcolor:
+                        value === "Open"
+                          ? "green" 
+                          : value === "Completed"
+                          ? "skyblue"
+                          : value === "On Hold"
+                          ? "red"
+                          : "orange",
+                      p: "0.5rem 1rem",
+                      borderRadius: 10,
+                    };
+                    break;
+                }
+
+                return (
+                  <StyledTableCell key={index} align={align || "left"}>
+                    <Typography
+                      variant="caption"
+                      sx={typographyStyles}
+                    >
+                      {content}
+                    </Typography>
+                  </StyledTableCell>
+                );
+              })}
             </StyledTableRow>
           ))}
         </TableBody>
