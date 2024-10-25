5e2f9e70e5b77464210855c7f00092941e7db139
luketomlinson13 <98561960+luketomlinson13@users.noreply.github.com>
2024-10-24 21:04:05 -0500

    emun logo added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Stack, Typography } from "@mui/material";
 import { CustomizedTables } from "@/components/Table";
 import { BasicSelect } from "../components/Select";
 import * as React from "react";
-
+import EmunLogo from "@/components/EmunLogo";
 export default function Home() {
   const [vendor, setVendor] = React.useState("All");
 
@@ -20,6 +20,7 @@ export default function Home() {
         
       <Stack gap={5} p={5}>
         <Stack>
+          <EmunLogo></EmunLogo>
           <Stack direction="row" sx={{ justifyContent: 'space-between', alignItems: 'center' }} ><Typography variant="h5" gap={2}>View Orders</Typography><BasicSelect vendor={vendor} vendorSelected={vendorSelected}></BasicSelect></Stack>
           <CustomizedTables vendor={vendor}/>
         </Stack>
