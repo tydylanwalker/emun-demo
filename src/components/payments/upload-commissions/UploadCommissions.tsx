7ae0491c43df8a196e41e36013c5f32546219b0b
Ty Walker <tydylanwalker@gmail.com>
2024-10-25 11:54:18 -0500

    pushing broken file upload (need to re-evaluate)

--- src/components/payments/upload-commissions/UploadCommissions.tsx
+++ src/components/payments/upload-commissions/UploadCommissions.tsx
@@ -1,8 +1,12 @@
 import { CustomInput } from "@/components/shared/CustomSelect";
 import { Button, Stack, Typography } from "@mui/material";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { AddCheck } from "./AddCheck";
 import { ICheckData } from "@/interfaces/ICheckData";
+import readExcelFile from "read-excel-file";
+import _ from "lodash";
+import { schema } from "../../../data/constants";
+
 
 export function UploadCommissions() {
     const vendorOptions = ['Option 1', 'Option 2', 'Option 3'];
@@ -17,6 +21,40 @@ export function UploadCommissions() {
         console.log('posted new check to db: ', checkToSave);
     }
 
+    const handleUploadFileClicked = async (e: ChangeEvent<HTMLInputElement>) => {
+        // const file = e.target.files ? e.target.files[0] : null;
+    
+        // e.preventDefault();
+    
+        // if (file) {
+        //   if (file.name.endsWith(".xlsx") || file.name.endsWith(".xls")) {
+        //     const fileData = (await readExcelFile(file, { schema, includeNullValues: true }).then(({ rows }) => {
+        //       // replace all null values with empty string
+        //       const cleanedRows = rows.map((row) => {
+        //         const newRow = _.mapValues(row, (v) => (v === null ? "" : v));
+        //         return newRow;
+        //       });
+        //       return cleanedRows;
+        //     }));
+    
+        //     const mappedFileData = fileData.map((row: any) => {
+        //       // Check for any errors in the row
+        //       const errors = checkFormErrors(row);
+    
+        //       // If any of them have error === 'true' then set row error to true
+        //       const hasError = Object.values(errors).includes(true);
+    
+        //       return {
+        //         ...row,
+        //         hasError: hasError,
+        //       };
+        //     });
+    
+        //     console.log(fileData)
+        //   }
+        // }
+      };
+
     return (
         <Stack gap={4}>
             <Typography variant="h6">{`Upload Commissions ${vendor && "for " + vendor}${check && " | " + check}`}</Typography>
@@ -25,6 +63,10 @@ export function UploadCommissions() {
                 <CustomInput select value={check} label="Select Check" options={checkOptions} onChange={(event) => setCheck(event.target.value as string)} endAction={<Button size="small" sx={{ color: 'grey', textTransform: 'none'}} onClick={() => setAddNewCheck(true)}>add new check +</Button>} />
             </Stack>
             {addNewCheck && <AddCheck open={addNewCheck} toggleDrawer={(open: boolean) => setAddNewCheck(open)} vendor={vendor} vendorOptions={vendorOptions} saveCheck={saveCheck}/>}
+            <Button variant="contained" component="label" sx={{mt: 10}}>
+                Upload File Test
+                <input type="file" accept=".xlsx, .xls" onChange={(e) => handleUploadFileClicked(e)} hidden />
+            </Button>
         </Stack>
     )
 }
